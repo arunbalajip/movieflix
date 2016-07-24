d248ea946f8c47f4360d6e8224f9211153f7bc64
arunbalaji prithiviraj <arunbalaji prithiviraj>
2016-07-23 22:33:13 -0400

    feat(module-api): init server module

---
+++ server/src/main/java/com/frygo/movieflix/server/App.java
@@ -0,0 +1,13 @@
+package com.frygo.movieflix.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
